3b6e521282a4cb32544e986cbf8f8743e7c9635f
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-04-30 21:22:28 +0530

    up

---
+++ tute2.py
@@ -0,0 +1,12 @@
+name= "akshitha"
+age= 22
+#output
+print("Name is "+name+ "Age is "+str(age))#method1
+print("Name is %s Age is %d" %(name,age))#method 2
+
+#input
+
+name1 = input("enetr your name")
+age1= int(input("enetr your age "))
+print(name1)
+print(age1)
