61b2b1281100924d53b29a090f16a21f4176008a
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-05-15 16:45:13 +0530

    final

--- hex.py
+++ hex.py
@@ -3,3 +3,11 @@ import turtle
 turtle.bgcolor("black")
 turtle.pensize(1)
 turtle.speed(0)
+
+
+for i in range(100):
+    for colours in ["red","magenta","blue","cyan","green","yellow"]:
+        turtle.color(colours)
+        turtle.circle(100)
+        turtle.left(10)
+turtle.hideturtle()
