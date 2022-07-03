bc75c6179713788ea8ab3b6891e0647e68c399dc
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-07-03 18:38:45 +0530

    Update animation.py

--- animation.py
+++ animation.py
@@ -33,3 +33,7 @@ catcher_starty2 = catcher_starty + catcher_height
 catcher = c.create_arc(catcher_startx, catcher_starty, catcher_startx2, catcher_starty2, start=200, extent=140, style="arc", outline=catcher_color, width=3)
 game_font = font.nametofont("TkFixedFont")
 game_font.config(size=18)
+
+
+score = 0
+score_text = c.create_text(10, 10, anchor="nw", font=game_font, fill="darkblue", text="Score: "+ str(score))
